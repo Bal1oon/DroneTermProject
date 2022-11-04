e153107da9c650e45d31e5d631c74a34ee01ebf1
Bal1oon <ws32159@naver.com>
2022-11-05 04:05:02 +0900

    test: 키보드 입력으로 실시간 비디오 캡처

---
+++ videoCaptureTest.py
@@ -0,0 +1,28 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+
+ret = False
+scale = 2
+
+i=0
+
+while True:
+    ret, frame = video.read()
+    if (ret):
+        # Our operations on the frame come here
+        height, width, layers = frame.shape
+        new_h = int(height / scale)
+        new_w = int(width / scale)
+        resize = cv2.resize(frame, (new_w, new_h))  # <- resize for improved performance
+        # Display the resulting frame
+        cv2.imshow('Tello', resize)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite(f"captures/test{i}.jpg", resize)  # writes image test.bmp to disk
+        print("Take Picture")
+        i+=1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
